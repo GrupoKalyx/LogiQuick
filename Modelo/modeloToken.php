811130a14fcee7750f7cbeef4cb14c66c0d170c7
Kalyx Software <kalyx4637@gmail.com>
2023-08-22 14:33:04 -0300

    v0.13.1

--- Modelo/modeloToken.php
+++ Modelo/modeloToken.php
@@ -10,23 +10,19 @@ class modeloToken extends modeloBd
 
     public function generateToken()
     {
-        if (function_exists('com_create_guid')) {
-            $new_GUID = com_create_guid();
-        } else {
-            mt_srand((float) microtime() * 10000);
-            //optional for php 4.2.0 and up.
-            $set_charid = strtoupper(md5(uniqid(rand(), true)));
-            $set_hyphen = chr(45);
-            // "-"
-            $set_uuid = chr(123)
-                . substr($set_charid, 0, 8) . $set_hyphen
-                . substr($set_charid, 8, 4) . $set_hyphen
-                . substr($set_charid, 12, 4) . $set_hyphen
-                . substr($set_charid, 16, 4) . $set_hyphen
-                . substr($set_charid, 20, 12)
-                . chr(125);
-            $new_GUID = $set_uuid;
-        }
+        mt_srand((float) microtime() * 10000);
+        //optional for php 4.2.0 and up.
+        $set_charid = strtoupper(md5(uniqid(rand(), true)));
+        $set_hyphen = chr(45);
+        // "-"
+        $set_uuid = chr(123)
+            . substr($set_charid, 0, 8) . $set_hyphen
+            . substr($set_charid, 8, 4) . $set_hyphen
+            . substr($set_charid, 12, 4) . $set_hyphen
+            . substr($set_charid, 16, 4) . $set_hyphen
+            . substr($set_charid, 20, 12)
+            . chr(125);
+        $new_GUID = $set_uuid;
         return $new_GUID;
     }
 
