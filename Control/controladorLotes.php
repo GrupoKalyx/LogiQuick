ab3b10108465bed7745d820d802405680611dffc
Kalyx Software <kalyx4637@gmail.com>
2023-09-19 21:17:03 -0300

    v0.16

---
+++ Control/controladorLotes.php
@@ -0,0 +1,79 @@
+<?php
+require '../Modelo/modeloLotes.php';
+class controladorLotes
+{
+    private $conn;
+
+    function __construct($conn)
+    {
+        $this->conn = $conn;
+    }
+
+    public function ingresar($context)
+    {
+        $numBulto = $context['post']['numBulto'];
+        $gmailCliente = $context['post']['gmailCliente'];
+        $fechaLlegada = $context['post']['fechaLlegada'];
+        $num = $context['post']['num'];
+        $calle = $context['post']['calle'];
+        $localidad = $context['post']['localidad'];
+        $departamento = $context['post']['departamento'];
+
+        modeloPaquetes::alta($numBulto, $gmailCliente, $fechaLlegada, $num, $calle, $localidad, $departamento, $this->conn);
+        header('location: ../../../Vista/indexAdministrador.php');
+    }
+
+    public function eliminar($context)
+    {
+        $numBulto = $context['post']['numBulto'];
+        modeloPaquetes::baja($numBulto, $this->conn);
+        header('location: ../../../Vista/indexAdministrador.php');
+    }
+
+    public function rastrear($context){
+        $ci = $context;
+        $result = modeloUsuarios::muestra($ci, $this->conn);
+        return $result;
+    }
+
+    public function listar($context)
+    {
+        $json = json_decode(modeloPaquetes::listado($this->conn));
+        $result = $json;
+
+        $arrayConsulta = array();
+
+        foreach ($result as $row) {
+            $numBulto = $row->numBulto;
+            $gmailCliente = $row->gmailCliente;
+            $idRastreo = $row->idRastreo;
+            $fechaLlegada = $row->fechaLlegada;
+            $fechaEntrega = $row->fechaEntrega;
+            $num = $row->num;
+            $calle = $row->calle;
+            $localidad = $row->localidad;
+            $departamento = $row->departamento;
+            array_push($arrayConsulta, ['Número de bulto: ' => $numBulto, '<br> Gmail del cliente: ' => $gmailCliente, '<br> ID de rastreo: ' => $idRastreo, '<br> Fecha de llegada: ' => $fechaLlegada, '<br> Fecha de entrega: ' => $fechaEntrega, '<br> Num: ' => $num, '<br> Calle: ' => $calle, '<br> Localidad: ' => $localidad, '<br> Departamento: ' => $departamento . '<br><br>']);
+        }
+
+        foreach ($arrayConsulta as $value) {
+            foreach ($value as $key => $v) {
+                echo "<a class='form__viewContent'> " . $key . " " . $v . "</a>";
+            }
+        }
+    }
+
+    public function modificar($context)
+    {
+        $numBulto = $context['post']['numBulto'];
+        $gmailCliente = $context['post']['gmailCliente'];
+        $fechaLlegada = $context['post']['fechaLlegada'];
+        $num = $context['post']['num'];
+        $calle = $context['post']['calle'];
+        $localidad = $context['post']['localidad'];
+        $departamento = $context['post']['departamento'];
+        
+        modeloPaquetes::modificacion($numBulto, $gmailCliente, $fechaLlegada, $num, $calle, $localidad, $departamento, $this->conn);
+        header('location: ../../../Vista/indexAdministrador.php');
+    }
+}
---
+++ Modelo/modeloLotes.php
@@ -0,0 +1,42 @@
+<?php
+
+class modeloLotes
+{
+
+    public static function alta($numBulto, $gmailCliente, $fechaLlegada, $num, $calle, $localidad, $departamento, $conn)
+    {
+        do {
+            $idRastreo = "";
+            for ($i = 0; $i < 16; $i++) { $idRastreo .= mt_rand(0, 9); }
+        } while (self::existe($idRastreo, $conn));
+        $query = "INSERT INTO paquetes(numBulto, gmailCliente, idRastreo, fechaLlegada, num, calle, localidad, departamento) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?))";
+        $conn->execute_query($query, [$numBulto, $gmailCliente, $idRastreo, $fechaLlegada, $num, $calle, $localidad, $departamento]);
+    }
+
+    public static function modificacion($numBulto, $gmailCliente, $fechaLlegada, $num, $calle, $localidad, $departamento, $conn)
+    {
+        $query = "UPDATE paquetes SET gmailCliente = ? AND fechaLlegada = ? AND num = ? AND calle = ? AND localidad = ? AND departamento = ? WHERE numBulto = ?";
+        $conn->execute_query($query, [$gmailCliente, $fechaLlegada, $num, $calle, $localidad, $departamento, $numBulto]);
+    }
+
+    public static function baja($ci, $conn)
+    {
+        $query = "DELETE FROM usuarios WHERE ci = ?";
+        $conn->execute_query($query, [$ci]);
+    }
+
+    public static function listado($conn)
+    {
+        $query = "SELECT * FROM almacenes";
+        $result = $conn->execute_query($query);
+        $result = $result->fetch_all(MYSQLI_ASSOC);
+        return json_encode($result);
+    }
+
+    public static function existe($numBulto, $conn){
+        $query = "SELECT * FROM paquetes WHERE numBulto = ?";
+        $result = $conn->execute_query($query, [$numBulto]);
+        $num = mysqli_num_rows($result);
+        return $num;
+    }
+}
