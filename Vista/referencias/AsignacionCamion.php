4f5fa54127082c0f4b1242cc0be64c0cf78bd12d
Kalyx Software <kalyx4637@gmail.com>
2023-09-27 09:08:16 -0300

    v2.0.3

--- Vista/referencias/AsignacionCamion.php
+++ Vista/referencias/AsignacionCamion.php
@@ -1,16 +1,4 @@
 <?php
-$server = "127.0.0.1";
-$user = "root";
-$password = "";
-$dbname = "logiquickbd";
-
-$conn = new mysqli($server, $user, $password, $dbname);
-if ($conn->connect_error) { die("Error en la conexión a la base de datos: " . $conn->connect_error); }
-?>
-
-<?php
-// ... código de conexión a la base de datos ...
-
 $sql = "SELECT matricula FROM camiones";
 $result = $conn->query($sql);
 
