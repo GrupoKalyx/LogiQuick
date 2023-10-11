ec1cb7d29c97f8ddb593ec2f76464b3a9979f45d
Kalyx Software <kalyx4637@gmail.com>
2023-10-11 14:54:51 -0300

    v2.6.2

--- Control/controladorCamiones.php
+++ Control/controladorCamiones.php
@@ -1,41 +1,41 @@
 <?php
-require '../Modelo/modeloLotes.php';
+require '../Modelo/modeloCamiones.php';
 
 $context = match ($_SERVER['REQUEST_METHOD']) {
-  'GET' => $_GET,
-  'POST' => $_POST,    
-  'PUT' => $_PUT,
-  'DELETE' => $_DELETE,
+        'GET' => $_GET,
+        'POST' => $_POST,
+        'PUT' => $_PUT,
+        'DELETE' => $_DELETE,
 };
 
 $function = $context['function'];
-controladorLotes::$function($context);
+controladorCamiones::$function($context);
 
-class controladorLotes
-{ 
+class controladorCamiones
+{
 
-    public static function ingresar($context)
-    {
-        $matricula = modeloLotes::alta();
-        echo $matricula;
-    }
+        public static function ingresar($context)
+        {
+                $matricula = modeloCamiones::alta();
+                echo $matricula;
+        }
 
-    public static function eliminar($context)
-    {
-        $matricula = $context['matricula'];
-        modeloLotes::baja($matricula);
-    }
+        public static function eliminar($context)
+        {
+                $matricula = $context['matricula'];
+                modeloCamiones::baja($matricula);
+        }
 
-    public static function listar($context)
-    {
-        $result = modeloLotes::listado();
-        echo $result;
-    }
-    
-    public static function modificar($context){
-        $matricula = $context['matricula'];
-        modeloLotes::modificacion($matricula);
-    }
+        public static function listar($context)
+        {
+                $result = modeloCamiones::listado();
+                echo $result;
+        }
+
+        public static function modificar($context)
+        {
+                $disponibilidad = $context['disponibilidad'];
+                $matricula = $context['matricula'];
+                modeloCamiones::modificacion($disponibilidad, $matricula);
+        }
 }
-
-
--- Modelo/modeloCamiones.php
+++ Modelo/modeloCamiones.php
@@ -1,47 +1,40 @@
-<?php
+o/mºi<?php
 
 require_once('modeloBd.php');
 
-class modeloLotes
+class modelocamiones
 {
     public static function alta()
     {
         $conn = modeloBd::conexion();
-        $query = "INSERT INTO lotes VALUES ();";
+        $query = "INSERT INTO camiones (matricula, disponibilidad) VALUES (?, ?);";
         $conn->execute_query($query);
-        $queryLastId = "SELECT LAST_INSERT_ID();";
-        $excLastId = $conn->execute_query($queryLastId);
-        $fetch = $excLastId->fetch_array(MYSQLI_ASSOC);
-        $result = $fetch['LAST_INSERT_ID()'];
         $conn->close();
-        return json_encode($result);
     }
 
-    public static function baja($idLote)
+    public static function baja($matricula)
     {
         $conn = modeloBd::conexion();
-        $query = "DELETE FROM lotes WHERE idLote = ?";
-        $conn->execute_query($query, [$idLote]);
+        $query = "DELETE FROM camiones WHERE matricula = ?";
+        $conn->execute_query($query, [$matricula]);
         $conn->close();
     }
 
     public static function listado()
     {
         $conn = modeloBd::conexion();
-        $query = "SELECT * FROM  lotes";
+        $query = "SELECT * FROM  camiones";
         $result = $conn->execute_query($query);
         $result = $result->fetch_all(MYSQLI_ASSOC);
         $conn->close();
         return json_encode($result);
     }
 
-    public static function modificacion()
+    public static function modificacion($disponibilidad, $matricula)
     {
         $conn = modeloBd::conexion();
-        $query = "SELECT * FROM  lotes";
-        $result = $conn->execute_query($query);
-        $result = $result->fetch_all(MYSQLI_ASSOC);
+        $query = "UPDATE camiones SET disponibilidad = ? WHERE matricula = ?";
+        $conn->execute_query($query, [$disponibilidad, $matricula]);
         $conn->close();
-        return json_encode($result);
     }
 }
