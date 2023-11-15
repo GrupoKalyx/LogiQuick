110315aca11e54ee909915e17c9bf564fec77bbd
Kalyx Software <137226228+GrupoKalyx@users.noreply.github.com>
2023-11-15 20:35:34 -0300

    fix ventanaEmergente

--- Vista/javascript/VentanaEmergente.js
+++ Vista/javascript/VentanaEmergente.js
@@ -102,53 +102,21 @@ document.getElementById('tracking__form').addEventListener('submit', async funct
         const resultado2 = await obtenerEstadoPaquete(idRastreo);
         const resultado3 = await obtenerDatosAlmacen(idRastreo);
 
-        function obtenerDatosPaquete(idRastreo) {
-            return fetch(`http://localhost/kalyx/Control/controladorPaquetes.php?function=rastrear&idRastreo=${idRastreo}`)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Error en la solicitud: ' + response.statusText);
-                    }
-                    return response.json();
-                });
-        }
-
-        function obtenerEstadoPaquete(idRastreo) {
-            return fetch(`http://localhost/kalyx/Control/controladorPaquetes.php?function=mostrarEstado&tipoId=idRastreo&idRastreo=${idRastreo}`)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Error en la solicitud: ' + response.statusText);
-                    }
-                    return response.json();
-                });
-        }
-
-        function obtenerDatosAlmacen(idRastreo) {
-            return fetch(`http://localhost/kalyx/Control/controladorAlmacenes.php?function=mostrarUltimo&idRastreo=${idRastreo}`)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Error en la solicitud: ' + response.statusText);
-                    }
-                    return response.json();
-                });
-        }
-
         var resultadoDiv = document.getElementById('resultado');
         var resultado2Div = document.getElementById('resultado2');
 
-        if (!resultado) {
-            resultadoDiv.textContent = "El paquete no existe o no está registrado aún.";
-        } else {
+        if (resultado && resultado !== 0) {
             var direccion = resultado.num + ' ' + resultado.calle + ', ' + resultado.departamento + ', Uruguay';
             var direccionAlmacen = resultado3.num + ' ' + resultado3.calle + ', ' + resultado3.departamento + ', Uruguay';
 
             resultadoDiv.textContent = "Dirección de entrega: " + direccion;
             resultado2Div.textContent = "Estado del Paquete: " + resultado2;
 
-            var latitudDestino = parseFloat(resultado.lat); // Usar el valor de latitud desde la tabla de paquetes
-            var longitudDestino = parseFloat(resultado.lng); // Usar el valor de longitud desde la tabla de paquetes
+            var latitudDestino = parseFloat(resultado.lat);
+            var longitudDestino = parseFloat(resultado.lng);
 
-            var latitudOrigen = parseFloat(resultado3.lat); // Usar el valor de latitud desde la tabla de almacenes
-            var longitudOrigen = parseFloat(resultado3.lng); // Usar el valor de longitud desde la tabla de almacenes
+            var latitudOrigen = parseFloat(resultado3.lat);
+            var longitudOrigen = parseFloat(resultado3.lng);
 
             var map = L.map('map').setView([latitudDestino, longitudDestino], 13);
 
@@ -173,16 +141,48 @@ document.getElementById('tracking__form').addEventListener('submit', async funct
             L.marker([latitudDestino, longitudDestino], { icon: iconoPersonalizadoPaquete }).addTo(map);
             L.marker([latitudOrigen, longitudOrigen], { icon: iconoPersonalizadoAlmacen }).addTo(map);
 
-            // L.Routing.control({
-            //     waypoints: [
-            //         L.latLng(latitudOrigen, longitudOrigen), // Coordenadas del almacén como punto de origen
-            //         L.latLng(latitudDestino, longitudDestino)
-            //     ],
-            //     routeWhileDragging: true,
-            //     show: false,
-            
+            // Resto del código para la inicialización del mapa
+        } else {
+            document.getElementById('ventanaEmergente').style.display = 'block';
+            document.getElementById('map').style.display = 'none';
+            document.querySelector('.ventana__cerrar').addEventListener('click', function () {
+                document.getElementById('ventanaEmergente').style.display = 'none';
+                document.getElementById('map').style.display = 'none';
+            });
+            resultadoDiv.textContent = "El paquete no existe o no está registrado aún.";
         }
     } catch (error) {
         console.error('Error en la solicitud:', error);
     }
 });
+
+function obtenerDatosPaquete(idRastreo) {
+    return fetch(`http://localhost/LogiQuick/Control/controladorPaquetes.php?function=rastrear&idRastreo=${idRastreo}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la solicitud: ' + response.statusText);
+            }
+            return response.json();
+        });
+}
+
+function obtenerEstadoPaquete(idRastreo) {
+    return fetch(`http://localhost/LogiQuick/Control/controladorPaquetes.php?function=mostrarEstado&tipoId=idRastreo&idRastreo=${idRastreo}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la solicitud: ' + response.statusText);
+            }
+            return response.json();
+        });
+}
+
+function obtenerDatosAlmacen(idRastreo) {
+    return fetch(`http://localhost/LogiQuick/Control/controladorAlmacenes.php?function=mostrarUltimo&idRastreo=${idRastreo}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la solicitud: ' + response.statusText);
+            }
+            return response.json();
+        });
+}
+
